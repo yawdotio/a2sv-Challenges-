30607f4841d89920d9fbb1cbc7845781ffc0dd3e
Yaw Adu Bempong <128329972+yawdotio@users.noreply.github.com>
2025-01-30 19:37:43 +0000

    Create leetcodepalindrome.py

---
+++ leetcodepalindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return True if (str(x) == str(x)[::-1]) else False
+        
